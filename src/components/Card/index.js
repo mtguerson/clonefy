496cfdb5721326c950c8098d98a2276430d3d909
Matheus Guerson <mtguerson@gmail.com>
2024-03-01 09:10:36 -0400

    integration to a simple API

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container >
       <header>
-        <Label color="#7159c1" />
+        {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
-      <p>Fazer migração completa de servidor</p>
-      <img src="https://www.outsystems.com/Forge_CW/_image.aspx/Q8LvY--6WakOw9afDCuuGb5f5icowa9YX3jiQCQX08s=/adorable-avatars-utility-2023-01-04%2000-00-00-2023-03-01%2003-33-11" alt=""/>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt=""/>}
     </Container>
   );
 }

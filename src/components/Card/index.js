a320a1610e6c64a66875aef03e10abe3ff9141f5
Matheus Guerson <mtguerson@gmail.com>
2024-03-01 10:52:09 -0400

    starting droping development

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { useDrag } from 'react-dnd';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index }) {
+  const ref = useRef();
+
   const [{ isDragging }, dragRef] = useDrag({
-    type: 'CARD',
+    item: {index}, type: 'CARD',
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
   });
 
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) {
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if (draggedIndex === targetIndex) {
+        return;
+      }
+
+      console.log('ok');
+    }
+  })
+
+  dragRef(dropRef(ref));
+
   return (
-    <Container ref={dragRef} isDragging={isDragging}>
+    <Container ref={ref} isDragging={isDragging}>
       <header>
         {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
